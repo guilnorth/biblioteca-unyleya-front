0f618ccd41681b23313a5a00becbec7974d7e68f
Guilherme <ggn.noronha@gmail.com>
2020-04-04 18:07:17 -0300

    Header and Drawer

---
+++ src/components/shared/Header.vue
@@ -0,0 +1,13 @@
+<template>
+  <v-app-bar app color="indigo" dark>
+    <v-app-bar-nav-icon @click.stop="$emit('toggle-drawer')" />
+    <v-toolbar-title>Biblioteca</v-toolbar-title>
+  </v-app-bar>
+</template>
+
+<script>
+export default {
+  name: "Header",
+  data: () => ({})
+};
+</script>
---
+++ src/components/shared/NavigationDrawer.vue
@@ -0,0 +1,31 @@
+<template>
+  <v-navigation-drawer v-model="drawer" app>
+    <v-list dense>
+      <v-list-item link>
+        <v-list-item-action>
+          <v-icon>mdi-home</v-icon>
+        </v-list-item-action>
+        <v-list-item-content>
+          <v-list-item-title>Livros</v-list-item-title>
+        </v-list-item-content>
+      </v-list-item>
+      <v-list-item link>
+        <v-list-item-action>
+          <v-icon>mdi-contact-mail</v-icon>
+        </v-list-item-action>
+        <v-list-item-content>
+          <v-list-item-title>Autores</v-list-item-title>
+        </v-list-item-content>
+      </v-list-item>
+    </v-list>
+  </v-navigation-drawer>
+</template>
+
+<script>
+export default {
+  name: "NavigationDrawer",
+  data: () => ({
+    drawer: true
+  })
+};
+</script>
