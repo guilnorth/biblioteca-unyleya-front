a6e3d96e6078717a92a489e84aae9524b1b5124b
Guilherme <ggn.noronha@gmail.com>
2020-04-05 01:45:14 -0300

    Authors pages

---
+++ src/views/authors/DetailAuthors.vue
@@ -0,0 +1,29 @@
+<template>
+  <FormDetail>
+    <template v-slot:detail>
+      <h1>Detalhes</h1>
+    </template>
+    <template v-slot:editForm>
+      <FormAuthors />
+    </template>
+  </FormDetail>
+</template>
+
+<script>
+import FormDetail from "../../components/FormDetail";
+import FormAuthors from "./FormAuthors";
+export default {
+  name: "DetailAuthors",
+  components: { FormDetail, FormAuthors },
+  data: () => ({
+    isEdit: false
+  }),
+  methods: {
+    goToEdit: function() {
+      this.$router.push("/edit");
+    }
+  }
+};
+</script>
+
+<style lang="scss" scoped></style>
---
+++ src/views/authors/ListAuthors.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="d-flex flex-wrap">
+    <div class="d-flex pa-6" v-for="n in authorsList" :key="n.id">
+      <CardBase :onHandleClick="onHandleItem" />
+    </div>
+  </div>
+</template>
+
+<script>
+import CardBase from "../../components/base/CardBase";
+import { mapActions, mapState } from "vuex";
+export default {
+  name: "ListAuthors",
+  props: ["onItemEvent"],
+  components: { CardBase },
+  mounted() {
+    this.getList();
+    //if (this.$router.currentRoute.params.id) this.$refs.dialog.dialog = true;
+  },
+  computed:{
+    ...mapState({
+      authorsList: state => state.authors.list,
+      //notification: state => state.users.data.notification
+    })
+  },
+  methods: {
+    ...mapActions("authors", ["getList"]),
+    onHandleItem: function() {
+      //Buscar os dados
+      //
+      return this.onItemEvent(12);
+    }
+  },
+  data: () => ({})
+};
+</script>
+
+<style lang="scss" scoped></style>
