872387373d0c170cb276488cdfb9b9771c09040e
Guilherme <ggn.noronha@gmail.com>
2020-04-05 01:43:10 -0300

    Generic Module Vuex

---
+++ src/store/modules/GenericModule.js
@@ -0,0 +1,126 @@
+import BaseService from "../services/BaseService";
+export default function(resource, objectModel) {
+  return {
+    namespaced: true,
+    state: {
+      detail: objectModel,
+      list: [],
+      loadingOnList: false,
+      loadingOnDetail: false,
+      loadingOnSave: false,
+      notification: {}
+    },
+    getters: {},
+    mutations: {
+      setList: (state, list) => {
+        state.list = list;
+      },
+      setDetail: (state, item) => {
+        state.detail = item;
+      },
+      setLoadingList(state, loading) {
+        state.loadingOnList = loading;
+      },
+      setLoadingDetail(state, loading) {
+        state.loadingOnDetail = loading;
+      },
+      setLoadingSave(state, loading) {
+        state.loadingOnDetail = loading;
+      },
+      setError(state, err) {
+        state.error = err;
+      },
+      setNotification(state, { type, message }) {
+        state.notification = { type, message, show: true };
+      }
+    },
+    actions: {
+      getList: async ({ commit }) => {
+        commit("setLoadingList", true);
+        try {
+          const result = await BaseService.getList(resource);
+          commit("setList", result.data);
+        } catch (error) {
+          console.log(error);
+          commit("setNotification", {
+            type: "error",
+            message: "Não foi possível buscar no momento."
+          });
+        }
+        commit("setLoadingList", false);
+      },
+      getOne: async ({ commit }, { id }) => {
+        commit("setLoadingDetail", true);
+        try {
+          const result = await BaseService.getOne(resource, id);
+          commit("setDetail", result.data);
+        } catch (error) {
+          console.log(error);
+          commit("setNotification", {
+            type: "error",
+            message: "Não foi possível buscar o autor no momento."
+          });
+        }
+        commit("setLoadingDetail", false);
+      },
+      save: async ({ commit }, { data }) => {
+        commit("setLoadingSave", true);
+        try {
+          const result = await BaseService.saveItem(resource, data);
+          commit("setDetail", result.data);
+          //router.back();
+          commit("setNotification", {
+            type: "success",
+            message: "Salvo com sucesso"
+          });
+        } catch (error) {
+          console.log(error);
+          commit("setNotification", {
+            type: "error",
+            message: "Não foi possível salvar no momento."
+          });
+        }
+        commit("setLoadingSave", false);
+      },
+      update: async ({ commit }, { data, id }) => {
+        commit("setLoadingSave", true);
+        try {
+          const result = await BaseService.updateItem(resource, data, id);
+          commit("setDetail", result.data);
+          //router.back();
+          commit("setNotification", {
+            type: "success",
+            message: "Atualizado com sucesso"
+          });
+        } catch (error) {
+          console.log(error);
+          commit("setNotification", {
+            type: "error",
+            message: "Não foi possível atualizar no momento."
+          });
+        }
+        commit("setLoadingSave", false);
+      },
+      delete: async ({ commit }, { data, id }) => {
+        commit("setLoadingSave", true);
+        try {
+          await BaseService.updateItem(resource, data, id);
+          commit("setDetail", objectModel);
+          //router.replace("/authors");
+          commit("setNotification", {
+            type: "success",
+            message: "Registro apagado"
+          });
+        } catch (error) {
+          console.log(error);
+          commit("setNotification", {
+            type: "error",
+            message:
+              "Não foi possível apagar o registro, verifique os item relacionados."
+          });
+        }
+        commit("setLoadingSave", false);
+      }
+    }
+  };
+}
