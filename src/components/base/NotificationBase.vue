bee544220ab2d8c257391ef0bbe65103ec36f82e
Guilherme <ggn.noronha@gmail.com>
2020-04-05 20:32:59 -0300

    Standart notification component

---
+++ src/components/base/NotificationBase.vue
@@ -0,0 +1,21 @@
+<template>
+  <v-snackbar
+    v-model="notification.show"
+    :color="notification.type"
+    multi-line="multi-line"
+    :top="true"
+    :timeout="6000"
+  >
+    {{ notification.message }}
+    <v-btn dark text @click="notification.show = false">Fechar</v-btn>
+  </v-snackbar>
+</template>
+
+<script>
+export default {
+  name: "NotificationBase",
+  props: ["notification"]
+};
+</script>
+
+<style lang="scss" scoped></style>
