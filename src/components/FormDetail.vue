e5ad55cde38bfa52ebe48817114973f464fa2d1d
Guilherme <ggn.noronha@gmail.com>
2020-04-05 01:47:08 -0300

    Form Detail base

---
+++ src/components/FormDetail.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <v-row v-if="!isEdit">
+      <slot name="detail"></slot>
+      <v-col cols="12" sm="12" md="12">
+        <v-btn @click="isEdit = true"> Editar</v-btn>
+      </v-col>
+    </v-row>
+    <v-row v-if="isEdit" >
+      <slot name="editForm"></slot>
+      <v-btn @click="isEdit = false"> Cancelar</v-btn>
+      <v-btn> Editar</v-btn>
+    </v-row>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "FormDetail",
+  //props:['endpoint'],
+  data: () => ({
+    isEdit: false
+  }),
+  methods: {}
+};
+</script>
+
+<style lang="scss" scoped></style>
