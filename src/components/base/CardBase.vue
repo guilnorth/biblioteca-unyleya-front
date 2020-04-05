441a9cffbedf4181b6f1861b67b27df16d17b225
Guilherme <ggn.noronha@gmail.com>
2020-04-05 20:34:48 -0300

    Card base component

--- src/components/base/CardBase.vue
+++ src/components/base/CardBase.vue
@@ -1,51 +1,53 @@
 <template>
   <v-card
     class="mx-auto"
-    color="#26c6da"
+    color="#4DD0E1"
     dark
     max-width="400"
     @click="onHandleClick()"
   >
-    <v-card-title>
-      <v-icon large left>
-        mdi-book
-      </v-icon>
-      <span class="title font-weight-light"></span>
+    <v-card-title style="background:#1976D2" class="pa-4">
+      <v-list-item-avatar color="primary darken-3" class="mr-4 ml-0">
+        <v-icon color="white dark" style="">
+          {{ iconCard }}
+        </v-icon>
+      </v-list-item-avatar>
+      <span class="title font-weight-light text-right pb-0">
+        {{ titleCard }}
+      </span>
     </v-card-title>
 
-    <v-card-text class="headline font-weight-bold">
-      O Senhor dos Anéis
+    <v-card-text class="headline font-weight-bold pa-4">
+      <slot name="cardContent"></slot>
     </v-card-text>
-
-    <!-- <v-card-actions>
-      <v-list-item class="grow">
-        <v-list-item-avatar color="grey darken-3">
-          <v-img
-            class="elevation-6"
-            src="https://avataaars.io/?avatarStyle=Transparent&topType=ShortHairShortCurly&accessoriesType=Prescription02&hairColor=Black&facialHairType=Blank&clotheType=Hoodie&clotheColor=White&eyeType=Default&eyebrowType=DefaultNatural&mouthType=Default&skinColor=Light"
-          ></v-img>
-        </v-list-item-avatar>
-
-        <v-list-item-content>
-          <v-list-item-title>Evan You</v-list-item-title>
-        </v-list-item-content>
-
-        <v-row align="center" justify="end">
-          <v-icon class="mr-1">mdi-heart</v-icon>
-          <span class="subheading mr-2">256</span>
-          <span class="mr-1">·</span>
-          <v-icon class="mr-1">mdi-share-variant</v-icon>
-          <span class="subheading">45</span>
-        </v-row>
-      </v-list-item>
-    </v-card-actions> -->
   </v-card>
 </template>
 
 <script>
 export default {
   name: "CardBase",
-  props: ["onHandleClick"]
+  props: {
+    onHandleClick: {
+      type: Function,
+      required: false,
+      default: function() {}
+    },
+    colorCard: {
+      type: String,
+      required: false,
+      default: "#26c6da"
+    },
+    titleCard: {
+      type: String,
+      required: false,
+      default: "Livro"
+    },
+    iconCard: {
+      type: String,
+      required: false,
+      default: "mdi-book-multiple"
+    }
+  }
 };
 </script>
 
