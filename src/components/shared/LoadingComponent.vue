dc7ef6a9c6807769c85de1cd02c37224801b84a2
Guilherme <ggn.noronha@gmail.com>
2020-04-05 01:46:14 -0300

    Skelleton element for loading

---
+++ src/components/shared/LoadingComponent.vue
@@ -0,0 +1,39 @@
+<template>
+  <v-col v-if="loading">
+    <v-sheet :color="color" :class="sheetClass">
+      <v-skeleton-loader
+        class="mx-auto"
+        :max-width="width"
+        :type="type"
+      ></v-skeleton-loader>
+    </v-sheet>
+  </v-col>
+</template>
+
+<script>
+export default {
+  name: "LoadingComponent",
+  props: {
+    loading: {
+      type: Boolean,
+      default: true
+    },
+    type: {
+      type: String,
+      default: "card"
+    },
+    sheetClass: {
+      type: String,
+      default: "px-3 pt-3 pb-3"
+    },
+    width: {
+      type: Number,
+      default: 400
+    },
+    color: {
+      type: String,
+      default: "lighten-4"
+    }
+  }
+};
+</script>
