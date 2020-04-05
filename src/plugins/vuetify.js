cba45cd5de833f3f838f2f8fd91390aa3838480a
Guilherme <ggn.noronha@gmail.com>
2020-04-05 20:31:53 -0300

    Add icon vuetify

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -3,4 +3,8 @@ import Vuetify from "vuetify/lib";
 
 Vue.use(Vuetify);
 
-export default new Vuetify({});
+export default new Vuetify({
+  icons: {
+    iconfont: "mdi" //|| 'mdiSvg' || 'md' || 'fa' || 'fa4' || 'faSvg'
+  }
+});
