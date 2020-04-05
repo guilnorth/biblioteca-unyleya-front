e87434d87195920ac759cf21e3fe9f926904fccb
Guilherme <ggn.noronha@gmail.com>
2020-04-05 20:32:18 -0300

    Navigation menus

--- src/components/shared/NavigationDrawer.vue
+++ src/components/shared/NavigationDrawer.vue
@@ -1,20 +1,12 @@
 <template>
   <v-navigation-drawer v-model="drawer" app>
     <v-list dense>
-      <v-list-item link>
+      <v-list-item v-for="menu in itemsMenu" :key="menu.title" :to="menu.link">
         <v-list-item-action>
-          <v-icon>mdi-home</v-icon>
+          <v-icon>{{ menu.icon }}</v-icon>
         </v-list-item-action>
         <v-list-item-content>
-          <v-list-item-title>Livros</v-list-item-title>
-        </v-list-item-content>
-      </v-list-item>
-      <v-list-item to="/authors">
-        <v-list-item-action>
-          <v-icon>mdi-contact-mail</v-icon>
-        </v-list-item-action>
-        <v-list-item-content>
-          <v-list-item-title>Autores</v-list-item-title>
+          <v-list-item-title>{{ menu.title }}</v-list-item-title>
         </v-list-item-content>
       </v-list-item>
     </v-list>
@@ -25,7 +17,13 @@
 export default {
   name: "NavigationDrawer",
   data: () => ({
-    drawer: true
+    drawer: true,
+    itemsMenu: [
+      { title: "Livros", link: "/books", icon: "mdi-book-multiple" },
+      { title: "Autores", link: "/authors", icon: "mdi-account-box" },
+      { title: "Editoras", link: "/publishers", icon: "mdi-home-modern" },
+      { title: "Gêneros", link: "/genres", icon: "mdi-palette" }
+    ]
   })
 };
 </script>
