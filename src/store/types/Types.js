91c9686cc240f958b1bb548db1cae4bae9d36ce5
Guilherme <ggn.noronha@gmail.com>
2020-04-05 01:45:35 -0300

    Model types

---
+++ src/store/types/Types.js
@@ -0,0 +1,28 @@
+const Author = {
+  name: "",
+  year_birth: null,
+  gender: "",
+  nationality: "",
+  id: null
+};
+
+const Book = {
+  title: "",
+  year: null,
+  author_id: null,
+  genre_id: null,
+  publisher_id: null,
+  id: null
+};
+
+const Genre = {
+  id: null,
+  name: ""
+};
+
+const Publisher = {
+  id: null,
+  name: ""
+};
+
+export { Author, Book, Genre, Publisher };
