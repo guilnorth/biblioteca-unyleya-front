0ac1f2db567eaf73305c0ede8c47b47d279bc852
Guilherme <ggn.noronha@gmail.com>
2020-04-05 01:42:40 -0300

    App base config and Axios

--- src/App.vue
+++ src/App.vue
@@ -3,28 +3,9 @@
     <NavigationDrawer ref="drawer" />
     <Header @toggle-drawer="$refs.drawer.drawer = !$refs.drawer.drawer" />
     <v-content>
-      <v-container class="fill-height" fluid>
+      <v-container class="" fluid>
         <v-row align="center" justify="center">
-          <v-col class="text-center">
-            <v-tooltip left>
-              <span>Source</span>
-            </v-tooltip>
-
-            <v-tooltip right>
-              <template v-slot:activator="{ on }">
-                <v-btn
-                  icon
-                  large
-                  href="https://codepen.io/johnjleider/pen/zgxeLQ"
-                  target="_blank"
-                  v-on="on"
-                >
-                  <v-icon large>mdi-codepen</v-icon>
-                </v-btn>
-              </template>
-              <span>Codepen</span>
-            </v-tooltip>
-          </v-col>
+          <router-view></router-view>
         </v-row>
       </v-container>
     </v-content>
@@ -40,7 +21,6 @@ export default {
   components: {
     Header,
     NavigationDrawer
-  },
- 
+  }
 };
 </script>
